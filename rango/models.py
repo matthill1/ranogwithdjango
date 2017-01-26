836b1b5a80abf54a903d1b489b323127752d4879
Matt Hill <2037856H@student.gla.ac.uk>
2017-01-26 13:56:47 +0000

    chapter 6

--- rango/models.py
+++ rango/models.py
@@ -1,19 +1,29 @@
-from __future__ import unicode_literals
 
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-
-    def __str__(self):  # For Python 2, use __unicode__ too
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
         return self.name
-
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-
-    def __str__(self):  # For Python 2, use __unicode__ too
+    
+    def __str__(self):
         return self.title
